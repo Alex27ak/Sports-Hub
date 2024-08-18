5b83c91c2026744b2b616d42c2d6d943d585119b
Alex27ak <96329236+Alex27ak@users.noreply.github.com>
2024-08-18 12:56:55 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -4,35 +4,35 @@ const User = require('../models/User');
 
 const router = express.Router();
 
-// Registration
-router.get('/register', (req, res) => {
-  res.render('register');
-});
-
-router.post('/register', async (req, res) => {
-  try {
-    const user = new User({ username: req.body.username });
-    await User.register(user, req.body.password);
-    res.redirect('/login');
-  } catch (err) {
-    res.redirect('/register');
-  }
-});
-
-// Login
 router.get('/login', (req, res) => {
-  res.render('login');
+    res.render('login');
 });
 
 router.post('/login', passport.authenticate('local', {
-  successRedirect: '/events',
-  failureRedirect: '/login',
+    successRedirect: '/',
+    failureRedirect: '/login'
 }));
 
-// Logout
+router.get('/register', (req, res) => {
+    res.render('register');
+});
+
+router.post('/register', (req, res) => {
+    const newUser = new User({ username: req.body.username });
+    User.register(newUser, req.body.password, (err, user) => {
+        if (err) {
+            console.log(err);
+            return res.render('register');
+        }
+        passport.authenticate('local')(req, res, () => {
+            res.redirect('/');
+        });
+    });
+});
+
 router.get('/logout', (req, res) => {
-  req.logout();
-  res.redirect('/');
+    req.logout();
+    res.redirect('/');
 });
 
 module.exports = router;
