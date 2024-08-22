b969d573e4ee7f7dba46de94ac03700092c057d6
Alex27ak <96329236+Alex27ak@users.noreply.github.com>
2024-08-22 09:54:36 +0530

    Update terms-popup.js

--- js/terms-popup.js
+++ js/terms-popup.js
@@ -1,3 +1,25 @@
-document.querySelector('.policy input').addEventListener('click', function() {
-  alert('Show your terms and conditions here.');
-});
+// Get the modal
+var popup = document.getElementById("terms-popup");
+
+// Get the button that opens the modal
+var btn = document.getElementById("terms-link");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    popup.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    popup.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == popup) {
+        popup.style.display = "none";
+    }
+}
